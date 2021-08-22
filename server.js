ecd22f7b0e837a0c48b37e6385ab1ef7238b3d55
DandyDo <alqasimtoon@gmail.com>
2021-08-22 18:10:30 -0400

    added profile base

--- server.js
+++ server.js
@@ -54,6 +54,24 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1]);
 });
 
+// Profile
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+
+    //check if user found in database
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    });
+
+    if(!found) {
+        res.status(404).json('User not found.');
+    }
+});
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
