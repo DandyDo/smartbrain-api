58e5a791165f15f55076fa01ff740e67ce6381dc
DandyDo <alqasimtoon@gmail.com>
2021-08-29 18:27:31 -0400

    updated the profile end point for db

--- server.js
+++ server.js
@@ -82,19 +82,21 @@ app.post('/register', (req, res) => {
 // Profile
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-
-    //check if user found in database
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    
+    db.select('*').from('users').where({id}).then(user => {
+        if (user.length) {
+            res.json(user[0]);
         }
-    });
+        else {
+            res.status(400).json('User not found.')
+        }
 
-    if(!found) {
-        res.status(400).json('User not found.');
-    }
+    })
+    .catch(() => res.status(400).json('There was a problem getting the user profile.'));
+
+    // if(!found) {
+    //     
+    // }res.status(400).json('User not found.');
 });
 
 // Image
